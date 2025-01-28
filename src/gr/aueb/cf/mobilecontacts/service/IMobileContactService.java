b4c191265aaee7628ebeb0098873cc7b57449a8d
Routsis Michail <mike7199@icloud.com>
2025-01-28 19:25:17 +0200

    updated

--- src/gr/aueb/cf/mobilecontacts/service/IMobileContactService.java
+++ src/gr/aueb/cf/mobilecontacts/service/IMobileContactService.java
@@ -13,8 +13,10 @@ public interface IMobileContactService {
    MobileContact insertMobileContact(MobileContactInsertDTO dto) throws PhoneNumberAlreadyExistsException;
    MobileContact updateMobileContact(MobileContactUpdateDTO dto)
             throws PhoneNumberAlreadyExistsException, ContactNotFoundException;
-
    void deleteContactById(Long id) throws ContactNotFoundException;
    MobileContact getContactById(Long id) throws ContactNotFoundException;
    List<MobileContact> getAllContacts();
+
+   MobileContact getContactByPhoneNumber(String phoneNumber) throws ContactNotFoundException;
+   void deleteContactByPhoneNumber(String phoneNumber) throws ContactNotFoundException;
 }
